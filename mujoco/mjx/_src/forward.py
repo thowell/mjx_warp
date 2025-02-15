9266bb9b9c9f880217b525047e12a2ff1aac3e69
Taylor Howell <taylorhowell@google.com>
2025-02-15 10:16:07 +0000

    format forward.py

--- mujoco/mjx/_src/forward.py
+++ mujoco/mjx/_src/forward.py
@@ -13,11 +13,11 @@ def fwd_velocity(m: types.Model, d: types.Data):
     worldid, actid, dofid = wp.tid()
     moment = d.actuator_moment[worldid, actid]
     qvel = d.qvel[worldid]
-    wp.atomic_add(d.actuator_velocity[worldid], actid, moment[dofid] * qvel[dofid])
+    wp.atomic_add(d.actuator_velocity[worldid],
+                  actid, moment[dofid] * qvel[dofid])
 
   wp.launch(_actuator_velocity, dim=(d.nworld, m.nu, m.nv), inputs=[d])
 
   # TODO(taylorhowell): smooth.com_vel
   passive.passive(m, d)
   smooth.rne(m, d)
-  
