631dc5cfd26e72b6ba9043121197974bd79944dd
Taylor Howell <taylorhowell@google.com>
2025-02-15 23:03:11 +0000

    fix __init__.py imports

--- mujoco/mjx/__init__.py
+++ mujoco/mjx/__init__.py
@@ -6,13 +6,12 @@ from ._src.io import make_data
 from ._src.io import put_data
 from ._src.io import put_model
 from ._src.passive import passive
-from ._src.smooth import kinematics
 from ._src.smooth import com_pos
+from ._src.smooth import com_vel
 from ._src.smooth import crb
 from ._src.smooth import factor_m
 from ._src.smooth import kinematics
 from ._src.smooth import rne
-from ._src.smooth import com_vel
 from ._src.support import is_sparse
 from ._src.test_util import benchmark
 from ._src.types import *
