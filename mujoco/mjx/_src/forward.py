52549b8d0db6b87534b16c8d5fd991aa6f6a281d
Taylor Howell <taylorhowell@google.com>
2025-02-20 15:32:32 +0000

    format forward.py

--- mujoco/mjx/_src/forward.py
+++ mujoco/mjx/_src/forward.py
@@ -226,6 +226,7 @@ def fwd_velocity(m: Model, d: Data):
 
   # TODO(team): tile operations?
   d.actuator_velocity.zero_()
+
   @wp.kernel
   def _actuator_velocity(d: Data):
     worldid, actid, dofid = wp.tid()
