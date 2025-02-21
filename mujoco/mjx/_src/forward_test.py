6b54e75582cffd1195757dcff55b87dbb1e28b72
Eric Heiden <eheiden@nvidia.com>
2025-02-21 08:47:18 -0800

    Apply nonzero ctrl, act in ForwardTest

--- mujoco/mjx/_src/forward_test.py
+++ mujoco/mjx/_src/forward_test.py
@@ -44,6 +44,8 @@ class ForwardTest(absltest.TestCase):
     mjd = mujoco.MjData(mjm)
     mujoco.mj_resetDataKeyframe(mjm, mjd, 1)  # reset to stand_on_left_leg
     mjd.qvel = np.random.uniform(low=-0.01, high=0.01, size=mjd.qvel.shape)
+    mjd.ctrl = np.random.normal(scale=10, size=mjd.ctrl.shape)
+    mjd.act = np.random.normal(scale=10, size=mjd.act.shape)
     mujoco.mj_forward(mjm, mjd)
     m = mjx.put_model(mjm)
     d = mjx.put_data(mjm, mjd)
@@ -65,17 +67,12 @@ class ForwardTest(absltest.TestCase):
     """Tests MJX fwd_actuation."""
     mjm, mjd, m, d = self._load("humanoid/humanoid.xml", is_sparse=False)
 
-    d.ctrl.fill_(100.0)
-    mjd.ctrl[:] = 100.0
     mujoco.mj_fwdActuation(mjm, mjd)
 
     for arr in (d.actuator_force, d.qfrc_actuator):
       arr.zero_()
 
     mjx.fwd_actuation(m, d)
-
-    print(d.ctrl.numpy())
-    print(d.qfrc_actuator.numpy())
 
     _assert_eq(d.ctrl.numpy()[0], mjd.ctrl, "ctrl")
     _assert_eq(d.actuator_force.numpy()[0], mjd.actuator_force, "actuator_force")
