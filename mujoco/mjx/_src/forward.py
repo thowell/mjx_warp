ebf14095bb2af65e73d304eba2c7db16e7ff3707
Kenny-Vilella <163487060+Kenny-Vilella@users.noreply.github.com>
2025-03-10 16:15:27 +0800

    Bugfix damping index in add_damping_sum_qfrc_kernel_sparse (#76)
    * [fixed] damping in add_damping_sum_qfrc_kernel_sparse
    
    * Improved fix

--- mujoco/mjx/_src/forward.py
+++ mujoco/mjx/_src/forward.py
@@ -173,7 +173,7 @@ def euler(m: Model, d: Data):
       worldId, tid = wp.tid()
 
       dof_Madr = m.dof_Madr[tid]
-      d.qM_integration[worldId, 0, dof_Madr] += m.opt.timestep * m.dof_damping[dof_Madr]
+      d.qM_integration[worldId, 0, dof_Madr] += m.opt.timestep * m.dof_damping[tid]
 
       d.qfrc_integration[worldId, tid] = (
         d.qfrc_smooth[worldId, tid] + d.qfrc_constraint[worldId, tid]
