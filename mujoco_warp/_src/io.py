cbb3fbb6314004eb44575226cea6f986bc1b418d
Taylor Howell <taylorhowell@google.com>
2025-03-18 15:32:56 +0000

    fix formatting

--- mujoco_warp/_src/io.py
+++ mujoco_warp/_src/io.py
@@ -24,7 +24,7 @@ from . import types
 def put_model(mjm: mujoco.MjModel) -> types.Model:
   if mjm.nsensor > 0:
     raise NotImplementedError("Sensors are unsupported.")
-  
+
   m = types.Model()
   m.nq = mjm.nq
   m.nv = mjm.nv
